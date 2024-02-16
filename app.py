405c31c815095cf30b558fc820c83cb44463075d
chegedan_ <danchege.w@outlook.com>
2024-02-16 08:24:20 +0300

    tal's code

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 import flask
-from flask import Flask,render_template,request,jsonify
+from flask import Flask,render_template,request,url_for, flash,jsonify
 from chat import get_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_socketio import SocketIO, emit, join_room
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DeNt24@CkOvWnAs'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_agent = db.Column(db.Boolean, default=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.get("/")
 def index_get():
